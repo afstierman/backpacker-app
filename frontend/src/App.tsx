22fac36b60cda4bc6066acad318b939fc987dca2
afstierman <austin.stierman@gmail.com>
2026-05-14 11:30:12 -0500

    Changed from mapbox to mapLibre

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import MapView from "./components/map/MapView";
+import StarfieldBackground from "./components/StarfieldBackground";
 
 function App() {
   return (
-    <>
+    <div className="relative w-screen h-screen">
+      <StarfieldBackground />
       <MapView />
-    </>
+    </div>
   );
 }
 
